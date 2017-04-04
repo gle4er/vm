8a7a5e2aae61c529c5bc66191fc05490e9971b78
Гриша Тарасенко <fle4er@yandex.ru>
2017-04-05 00:25:16 +0700

    Работа над ошибками

--- lab2/main.c
+++ lab2/main.c
@@ -6,9 +6,12 @@ int main()
     mt_clrscr();
     int rows, cols;
     mt_getscreensize(&rows, &cols);
+    rows /= 2;
     cols /= 2;
     cols -= 10;
-    mt_gotoXY(cols, 0);
+    mt_gotoXY(cols, rows);
     printf("Valerie Funtikoffa\n");
+    mt_getscreensize(&rows, &cols);
+    mt_gotoXY(cols, rows);
     return 0;
 }
